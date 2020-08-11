8fa6355cd5d95f4997ca596775a1f17e5a2991b1
Hamid Alavi <ultihamid@gmail.com>
2020-08-12 01:42:09 +0430

    update content

--- practice.js
+++ practice.js
@@ -3816,3 +3816,33 @@ ArrayBuffer {
 //     this.makeRequest();
 //   }
 // };
+
+
+// let controller = {
+//   makeRequest: function () {
+//     var self = this;
+//     btn.addEventListener("click", function () {
+//       // ..
+//       self.makeRequest();
+//     }, false);
+//   }
+// };
+
+// -----------------------------------------------
+
+// getter / setter
+// let o = {
+//   __id: 10,
+//   get id() { return this.__id++; },
+//   set id(v) { this.__id = v; }
+// }
+
+// console.log(o.id); // 10
+// console.log(o.id); // 11
+
+// o.id = 20;
+// console.log(o.id); // 20
+
+// // and:
+// console.log(o.__id); // 21
+// console.log(o.__id); // 21 -- still!
