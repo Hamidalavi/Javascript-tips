f1684a96b87513e036af0aea1962324a25c2c54b
Hamid Alavi <ultihamid@gmail.com>
2020-08-14 11:09:17 +0430

    update content

--- practice.js
+++ practice.js
@@ -4516,12 +4516,50 @@ ArrayBuffer {
 // console.log(it2.next()); // { value: [ 'hamid', 23 ], done: false }
 // console.log(it2.next()); // { value: [ { isIt: true }, 'exist' ], done: false }
 
+// -----------------------------------------------
 
+// iterator - loop
 // let it = {
 //   // make the `it` iterator an iterable
 //   [Symbol.iterator]() { return this; },
 //   next() { },
-
 // };
 
 // console.log(it[Symbol.iterator]() === it); // true
+
+
+// for (let v of it) {
+//   console.log(v);
+// }
+
+// iterator - custom
+// let Fib = {
+//   [Symbol.iterator]() {
+//     let n1 = 1, n2 = 1;
+
+//     return {
+//       // make the iterator an iterable
+//       [Symbol.iterator]() { return this; },
+
+//       next() {
+//         let current = n2;
+//         n2 = n1;
+//         n1 = n1 + current;
+//         return { value: current, done: false };
+//       },
+
+//       return(v) {
+//         console.log(
+//           "Fibonacci sequence abandoned."
+//         );
+//         return { value: v, done: true };
+//       }
+//     };
+//   }
+// };
+
+// for (let v of Fib) {
+//   console.log(v); // 1 1 2 3 5 8 13 21 34 55
+
+//   if (v > 50) break;
+// }
