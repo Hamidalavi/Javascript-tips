7e949818dcbe9e82641fad8272e3a270696c18ce
Hamid Alavi <ultihamid@gmail.com>
2020-08-18 04:25:53 +0430

    update

--- practice.js
+++ practice.js
@@ -6225,3 +6225,31 @@ ArrayBuffer {
 
 // console.log(a instanceof Hamid); // true
 // console.log(b instanceof Hamid); // false
+
+// -----------------------------------------------
+
+// meta programming - Symbol.species
+// class Cool {
+//   // defer `@@species` to derived constructor
+//   static get [Symbol.species]() { return this; }
+
+//   again() {
+//     return new this.constructor[Symbol.species]();
+//   }
+// }
+
+// class Fun extends Cool { }
+
+// class Awesome extends Cool {
+//   // force `@@species` to be parent constructor
+//   static get [Symbol.species]() { return Cool; }
+// }
+
+// let a = new Fun(),
+//   b = new Awesome(),
+//   c = a.again(),
+//   d = b.again();
+
+// console.log(c instanceof Fun); // true
+// console.log(d instanceof Awesome); // false
+// console.log(d instanceof Cool); // true
